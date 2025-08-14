1eebeb046924259f25ca2e2e6100bd3b3b6c1bdf
PauloStraforini <paulostraforini@gmail.com>
2025-08-14 16:29:40 -0300

    delete

--- src/pages/_layouts/AuthLayout.tsx
+++
@@ -1,9 +0,0 @@
-export const AuthLayout = ({ children }: { children?: React.ReactNode }) => {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-red-800">
-      <div className="w-full max-w-md bg-red rounded-lg shadow p-8">
-        {children}
-      </div>
-    </div>
-  )
-}
--- src/pages/app/dahsboard.tsx
+++
@@ -1,5 +0,0 @@
-export function Dashboard() {
-    return <h1>
-        salve
-    </h1>
-}
